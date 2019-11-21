acfd98fdf350056e2239409d3c723d1080d57fc1
Anton <zalaldinov@icloud.com>
2019-11-21 17:53:51 +0300

    Updates

---
+++ precache-manifest.f06b431c1502b724af2796512d53d579.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6977ce388c625917f9e40087d3083c57",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "87ba156883983a26cfff",
+    "url": "/deploy-test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a448853c02f763546227",
+    "url": "/deploy-test/static/js/2.f451acf8.chunk.js"
+  },
+  {
+    "revision": "87ba156883983a26cfff",
+    "url": "/deploy-test/static/js/main.dc4b0d3d.chunk.js"
+  },
+  {
+    "revision": "1e17d8d59077cf2b79cd",
+    "url": "/deploy-test/static/js/runtime-main.d38572b3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deploy-test/static/media/logo.25bf045c.svg"
+  }
+]);
